f55ee9f5c5ba7f90196db702c43bde0e3f6a6b2d
Su Liang <suliang1998@gmail.com>
2019-12-05 20:29:55 +0000

    Modify views to list all squirrles
    Create new template, add it to urlpattern, and modify views accordingly

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Squirrel
 
-# Create your views here.
+def all_squirrels(request):
+    squirrels = Squirrel.objects.all()
+    context = {
+            'squirrels': squirrels,
+            }
+    return render(request, 'sightings/all.html', context)
