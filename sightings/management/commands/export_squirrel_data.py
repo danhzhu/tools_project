c6095dbbc0825dc79c3461ff5d0df310cf49f976
Su Liang <suliang1998@gmail.com>
2019-12-06 19:12:20 +0000

    Write command to export csv

---
+++ sightings/management/commands/export_squirrel_data.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+from sightings.models import Squirrel
+import csv
+
+class Command(BaseCommand):
+    help = 'Export from db into csv'
+
+    def add_arguments(self, parser):
+        parser.add_argument('path', help = 'Get the file path')
+
+    def handle(self, *args, **options):
+        path = options['path']
+        csvfile = open(path, 'w')
+        writer = csv.writer(csvfile)
+        writer.writerow(['Longitude', 'Latitude', 'Shift', 'Date',
+            'Age', 'Primary Fur Color', 'Location', 'Specific Location',
+            'Running', 'Chasing', 'Climbing', 'Eating', 'Foraging',
+            'Other Activities', 'Kuks', 'Quaas', 'Moans', 'Tail flags',
+            'Tail twitches', 'Approaches', 'Indifferernt', 'Runs from'])
+        for obj in Squirrel.objects.all():
+            row = []
+            for field in Squirrel._meta.fields:
+                row.append(getattr(obj, field.name))
+            writer.writerow(row)
+        csvfile.close()
